d868a188ad6764899113e69ac0fb0c28b73d981a
Ved <bbbbaisayan@gmail.com>
2026-02-11 22:17:22 +0530

    removed unwanted rediskeys

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,9 +1,3 @@
 import { Redis } from "@upstash/redis";
 
 export const redis = Redis.fromEnv();
-
-export const RedisKeys = {
-  roomMeta: (roomId: string) => `meta:${roomId}`,
-  roomMessages: (roomId: string) => `messages:${roomId}`,
-  roomStream: (roomId: string) => `${roomId}`,
-} as const;
